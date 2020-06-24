1036f9288936e92a3459abcd1c480a70acac9f8d
adetiamarhadi <adetia.marhadi@gmail.com>
2020-06-24 10:25:19 +0700

    Chapter 96 : URI Customization

--- src/test/java/com/github/adetiamarhadi/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/com/github/adetiamarhadi/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -39,7 +39,7 @@ import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
-@AutoConfigureRestDocs
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "apps.host", uriPort = 80)
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "com.github.adetiamarhadi.sfgrestdocsexample.web.mappers")
 class BeerControllerTest {
